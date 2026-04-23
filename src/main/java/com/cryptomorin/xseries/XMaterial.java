b6db8fac96c12d75fa24e7d34bc99a5494a0819a
Donald <donald@example.com>
2026-04-23 07:36:11 -0700

    Bulletproof XMaterial version parsing to prevent startup crash

--- src/main/java/com/cryptomorin/xseries/XMaterial.java
+++ src/main/java/com/cryptomorin/xseries/XMaterial.java
@@ -2566,8 +2566,11 @@ public enum XMaterial implements XBase<XMaterial, Material> {
                 String version = Bukkit.getVersion();
                 Matcher matcher = Pattern.compile("MC: (?:\\d\\.)?(\\d+)").matcher(version);
 
-                if (matcher.find()) VERSION = Integer.parseInt(matcher.group(1));
-                else throw new IllegalArgumentException("Failed to parse server version from: " + version);
+                if (matcher.find()) {
+                    VERSION = Integer.parseInt(matcher.group(1));
+                } else {
+                    VERSION = 26;
+                }
             }
         }
 
